e40dcfe83f99d45898bb73223beb85c061626514
yatender-oktalk <yatender.nitk@gmail.com>
2020-05-01 08:39:50 +0530

    Fixed test cases

---
+++ test/rumbl/accounts_test.exs
@@ -0,0 +1,22 @@
+defmodule Rumbl.AcccoutsTest do
+  use Rumbl.DataCase, async: true
+
+  alias Rumbl.Accounts
+  alias Rumbl.Acccouts.User
+
+  describe "register_user/1" do
+    @valid_attrs %{
+      name: "User",
+      username: "eva",
+      password: "secret"
+    }
+    @invalid_attrs %{}
+
+    test "with valid data inserts user" do
+      assert {:ok, %User{id: id} = user} = Accounts.register_user(@valid_attrs)
+      assert user.name == "User"
+      assert user.username == "eva"
+      assert [%User{id: ^id}] = Accounts.list_users()
+    end
+  end
+end
