769f4cc66d59c51387f584c26840da1db36e6999
yatender-oktalk <yatender.nitk@gmail.com>
2020-10-12 10:57:39 +0530

    formatting the app.html.eex

--- config/dev.exs
+++ config/dev.exs
@@ -2,8 +2,8 @@ use Mix.Config
 
 # Configure your database
 config :rumbl, Rumbl.Repo,
-  username: "yatender",
-  password: "",
+  username: "blockfiadmin",
+  password: "blockfiadmin",
   database: "rumbl_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
